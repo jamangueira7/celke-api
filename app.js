c1de79402c78a3164a8058922242387d7c933e5a
João Mangueira <jamjs7@gmail.com>
2021-03-03 19:13:41 -0300

    conexão com o banco feitas

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(
+    'mongodb://localhost:27017/celke',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+    ).then(() => {
+        console.log("Conexão com o DB MongoDB realizado com sucesso!");
+    }).catch((err) => {
+        console.log("Erro: conexão deu pau!");
+    });
 
 app.get('/home', function (req, res) {
     res.send('Hello World');
