17ee7f63b7306d16dcb92ebca5a91808a490143e
João Mangueira <jamjs7@gmail.com>
2021-03-03 18:48:33 -0300

    add primeira rota e add nodemon

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
 app.listen(3333, function () {
     console.log("Servidor iniciado em http://localhost:3333/");
 });
