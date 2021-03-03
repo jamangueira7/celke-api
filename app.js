04bf39d4d2dab92a24aeee212ef140221ce26128
João Mangueira <jamjs7@gmail.com>
2021-03-03 19:40:56 -0300

    criação funcionando.

--- app.js
+++ app.js
@@ -15,13 +15,63 @@ mongoose.connect(
         console.log("Erro: conexão deu pau!");
     });
 
-app.get('/home', function (req, res) {
-    res.send('Pegar dados Home');
+app.get('/home', async (req, res) => {
+    await Home.findOne({}).then((home) => {
+        return res.json({
+            error: false,
+            home
+        });
+    }).catch((err) => {
+        res.status(400).json({
+            error: true,
+            message: "Nenhum registro encontrado!"
+        });
+    });
 });
 
-app.post('/home', function (req, res) {
-    res.send('Cadastrar pagina home');
+app.post('/home', async (req, res) => {
+    const dados = {
+        "topTitulo": "Temos a solução que usa empresa precisa!",
+        "topSubtitulo": "This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.",
+        "topTextoBtn": "Orçamento",
+        "topLinkBtn": "laptop-code",
+        "serTitulo": "Serviços",
+        "serSubtitulo": "This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.",
+        "serUmIcon": "",
+        "serUmTitulo": "Serviço um",
+        "serUmDesc": "This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.",
+        "serDoisIcon": "mobile-alt",
+        "serDoisTitulo": "Serviço dois",
+        "serDoisDesc": "This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.",
+        "serTresIcon": "network-wired",
+        "serTresTitulo": "Serviço três",
+        "serTresDesc": "This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.",
+    };
+
+    const homeExists = await Home.findOne({});
+
+    if(homeExists) {
+        return res.status(400).json({
+            error: true,
+            message: "Erro: A página Home já possui um registro no banco!"
+        });
+    }
+
+    await Home.create(dados, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: "Erro: Conteúdo da página não cadastrado com sucesso!"
+        });
+    });
+
+    return res.json({
+        error: false,
+        message: "Conteúdo da página home cadastrado com sucesso!"
+    });
+
 });
+
+
 
 app.listen(3333, function () {
     console.log("Servidor iniciado em http://localhost:3333/");
